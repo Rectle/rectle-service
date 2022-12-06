b318accbb98404bf7c660781b97b7a915a66f147
Piotr <piotrkopecki01@gmail.com>
2022-12-06 21:24:01 +0100

    improved code style

--- src/main/java/com/rectle/file/FilesController.java
+++ src/main/java/com/rectle/file/FilesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
-@RequestMapping("/${api.url}" + "/files")
+@RequestMapping("/${api.url}/files")
 @RestController
 public class FilesController {
 
