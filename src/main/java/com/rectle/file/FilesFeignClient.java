8c907d5d914b38628caf17fb2139662a090c1e1a
Piotr <piotrkopecki01@gmail.com>
2023-01-21 13:08:38 +0100

    fixed mechanism with sending post request to rectle-tasks-service in order to compile project

--- src/main/java/com/rectle/file/FilesFeignClient.java
+++ src/main/java/com/rectle/file/FilesFeignClient.java
@@ -1,13 +1,12 @@
 package com.rectle.file;
 
+import com.rectle.project.dto.ProjectToCompileDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(value = "fileFeignClient", url = "${rectle.tasks.service.url}")
 public interface FilesFeignClient {
 
-	@RequestMapping(method = RequestMethod.POST, value = "/")
-	void postForCompileFile(@RequestBody String task);
+	@PostMapping
+	void postForCompileFile(ProjectToCompileDto projectToCompileDto);
 }
