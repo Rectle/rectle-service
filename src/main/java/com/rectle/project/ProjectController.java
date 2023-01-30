b7628b51eae372521a8a6266e49ff05af00a0222
Piotr <piotrkopecki01@gmail.com>
2023-01-30 22:06:47 +0100

    changed upload project mechanism, now dto with project id and name is returned in purpose of temporary easy access to compile project

--- src/main/java/com/rectle/project/ProjectController.java
+++ src/main/java/com/rectle/project/ProjectController.java
@@ -1,8 +1,10 @@
 package com.rectle.project;
 
 import com.rectle.file.FilesUtils;
+import com.rectle.project.dto.UploadedProjectDto;
 import com.rectle.project.model.Project;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
@@ -15,23 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/${api.url}/projects")
 @RestController
 public class ProjectController {
 	private final ProjectService projectService;
+	private final ProjectDtoMapper projectDtoMapper;
 
 	@PostMapping("/{userId}")
-	public ResponseEntity<String> uploadProject(@RequestParam("file") MultipartFile multipartFile, @PathVariable Long userId) {
+	public ResponseEntity<UploadedProjectDto> uploadProject(@RequestParam("file") MultipartFile multipartFile, @PathVariable Long userId) {
 		if (!multipartFile.isEmpty() && multipartFile.getOriginalFilename() != null) {
 			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-			projectService.uploadProjectToCloudStorage(fileName, userId, multipartFile);
-			return new ResponseEntity<>(FilesUtils.SUCCESSFULLY_UPLOADED_MSG, HttpStatus.CREATED);
+			Project project = projectService.uploadProjectToCloudStorage(fileName, userId, multipartFile);
+			log.info(FilesUtils.SUCCESSFULLY_UPLOADED_MSG);
+			return new ResponseEntity<>(projectDtoMapper.projectToUploadedProjectDto(project), HttpStatus.CREATED);
 		}
-		return new ResponseEntity<>(FilesUtils.EMPTY_FILE_MSG, HttpStatus.BAD_REQUEST);
+		log.warn(FilesUtils.EMPTY_FILE_MSG);
+		return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 	}
 
-	@PutMapping
-	public ResponseEntity<String> compileProject(@RequestParam("projectId") Long projectId) {
+	@PutMapping("/{projectId}")
+	public ResponseEntity<String> compileProject(@PathVariable("projectId") Long projectId) {
 		Project project = projectService.findProjectById(projectId);
 		projectService.requestForCompilingProject(project.getId());
 		return new ResponseEntity<>(FilesUtils.SUCCESSFULLY_REQUESTED_FOR_COMPILE, HttpStatus.OK);
---
+++ src/main/java/com/rectle/project/dto/UploadedProjectDto.java
@@ -0,0 +1,11 @@
+package com.rectle.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UploadedProjectDto {
+	private Long id;
+	private String name;
+}
